1587bf37d40e1d67d0c39faa3c9dd3b3a1a3b627
Alexsandro Moraes de Brito <154477172+lexmoraes@users.noreply.github.com>
2024-10-17 21:24:37 -0400

    class.ts

--- exemploClasse.ts
+++ exemploClasse.ts
@@ -7,10 +7,19 @@ class Pessoa {
     this.idade =;
   }
 
-  saudar() {
+  saudar(pessoa: Pessoa): string {
     return 'Olá, meu nome é ${this.nome} e eu tenho ${this.idade} anos.'
   }
 }
 
 const pessoa = new Pessoa('João', 25);
 console.log(pessoa.saudar());
+
+'''Declaração com tipagens'''
+let nome: string = 'Alice';
+let idade: number = 30;
+let ativo: boolean = true;
+
+'''Funções com tipagens'''
+function saudar(pessoa: Pessoa): string {
+}
